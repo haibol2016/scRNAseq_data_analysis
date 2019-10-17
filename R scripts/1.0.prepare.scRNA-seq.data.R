5c13480a2cfbce0c8235c67dfe20c1f66c88360f
Haibo Liu <48487183+haibol2016@users.noreply.github.com>
2019-10-17 16:10:21 -0500

    update to CR3.0

--- R scripts/1.0.prepare.scRNA-seq.data.R
+++ R scripts/1.0.prepare.scRNA-seq.data.R
@@ -1,7 +1,5 @@
-
 ## load R 3.5 on nova 
 ## module load r/3.5.0-py2-qqwf6c6
-
 
 ## install core packages from Bioconductor
 if (!requireNamespace("BiocManager"))
@@ -9,8 +7,10 @@ if (!requireNamespace("BiocManager"))
 BiocManager::install()
 
 ## install all dependent packages
-packages2install <- c("dplyr", "cellrangerRkit", "cowplot", "knitr", "scran", "limma", "Seurat")
-uninstalled_packages <- packages2install[!packages2install %in% rownames(installed.packages())]
+packages2install <- c("dplyr", "cellrangerRkit", "cowplot", 
+                      "knitr", "scran", "limma", "Seurat")
+uninstalled_packages <- packages2install[!packages2install 
+                                         %in% rownames(installed.packages())]
 lapply(uninstalled_packages, BiocManager::install)
 
 ## load packages
@@ -18,57 +18,58 @@ packages2load <- c("cellrangerRkit", "dplyr", "Seurat")
 lapply(packages2load, library, character.only = TRUE)
 
 # Script to prepare cellranger data for downstream analysis
-
-# ---- ReadData ----
 # Read in output from cell ranger using cellrangerRkit
-samples <- c("./PBMC1", "./PBMC2")
+samples <- c("./A", "./B", "./C", "./CT2-1NOV", "./CT2-30OCT")
 library_id <- gsub(".+\\/", "", samples, perl = TRUE)
 
+## For CellRanger 3.x or 2.x, use a named vector specifying the path to 10X output 
+## for further analysis Within each folder, there are three .gz files for CellRanger V2.x, 
+## and four uncompressed files for CellRanger V3.x. The name will be used as prefix 
+## of cell barcodes in Seurat object
 
-## for CellRanger 3.x or 2.x
-## use a named vector specifying the path to 10X output for further analysis
-## Within each folder, there are three .gz files for CellRanger V2.x, 
-## and four uncompressed files for CellRanger V3.x
-
-data_dir <- c(PBMC1="C:\\Users\\Haibo\\Downloads\\PBMC1\\outs\\filtered_feature_bc_matrix",
-              PBMC2="C:\\Users\\Haibo\\Downloads\\PBMC2\\outs\\filtered_feature_bc_matrix")
-
-numberOfCellperLib <- vector("numeric", length(samples))
+data_dir <- file.path(getwd(),library_id, "outs/filtered_feature_bc_matrix")
+names(data_dir) <- library_id
 
 ## Whether the output is from CellRanger v2.x or CellRanger v3.x
-pre_version3.0 <- TRUE  # CellRanger v2.x
-
+pre_version3.0 <- FALSE  # CellRanger v2.x
 
 ##### For output from CellRanger v2.x
 if (pre_version3.0) 
 {
     gene_bc_matrix <- lapply(samples, load_cellranger_matrix, genome="ssc3")
-
+    
     ## Cell Barcode
     pDat <- do.call(rbind, lapply( gene_bc_matrix, function(.x){
         df <- data.frame(pData(.x))
         print(dim(df))
         df
     }))
-
+    
     ## number of detected cells
     numberOfCellperLib <- do.call(c, lapply(gene_bc_matrix, function(.x){
         nrow(pData(.x))
     }))
-
+    
     ## count table
     cDat <- do.call(cbind, lapply(gene_bc_matrix, function(.x){
         df <-as.matrix(exprs(.x))
         df <- df[order(rownames(df)), ]
         df
     }))
-
+    
     # reduce size of matrix
     keep <- rowSums(cDat) > 1
     cDat <- cDat[keep,]   ##  13064 genes,  8767 cells
-
+    
     ## gene symbol mapping
     fDat <- data.frame(fData(gene_bc_matrix[[1]]))[rownames(cDat),]
+    
+    # Add more info to phenotype Data
+    pDat <- mutate(pDat, barcode=as.character(barcode)) %>%
+        mutate(SampleID= do.call(c, mapply(function(.x, .y){
+            rep(.x, .y)
+        }, library_id, numberOfCellperLib, SIMPLIFY = FALSE)))
+    
 } else {
     lapply(data_dir, dir) ## Should show barcodes.tsv, genes.tsv, and matrix.mtx
     scRNA_data <- Read10X(data.dir = data_dir)
@@ -76,48 +77,47 @@ if (pre_version3.0)
     
     ## Cell Barcode
     pDat <-data.frame(barcode = colnames(seurat_object))
-    ## number of cells detected from each dataset
-    numberOfCellperLib <- do.call(c, lapply(names(data_dir), function(.x) {
-        sum(grepl(pattern = .x, colnames(seurat_object), fixed = TRUE))
-    }))
+    pDat$SampleID <- gsub("([^_]+).+", "\\1", pDat$barcode, perl = TRUE)
+    
     ## count table
     cDat <-  as.matrix(GetAssayData(object = seurat_object, slot = 'counts'))
     # reduce size of matrix
     keep <- rowSums(cDat) > 1
     cDat <- cDat[keep,]   ##  13064 genes,  8767 cells
     ## gene symbol mapping
-    fDat <- data.frame(id = rownames(cDat))
-    rownames(fDat) <- fDat$id  
-}    
+    fDat <- data.frame(ID = rownames(cDat))
+    rownames(fDat) <- fDat$ID 
+    
+    ## augmented fDat by adding gene symbols
+    con <- gzfile(file.path(data_dir[1], "features.tsv.gz"))
+    ssc_genes <- read.delim(con, sep = "\t", header = FALSE, as.is = TRUE)[, 1:2]
+    colnames(ssc_genes) <- c("EnsemblID", "Symbol")
+    ssc_genes$Symbol<- gsub("_", "-", ssc_genes$Symbol, perl = TRUE)
+    
+    fDat <- merge(fDat, ssc_genes, by.x ="row.names", 
+                  by.y = "Symbol", all.x =TRUE, sort = FALSE)
+    rownames(fDat) <- fDat[, 1]
+    fDat <- fDat[, -1]
 
-
-# ---- Formatting ----
-
-# Add more info to phenotype Data
-pDat <- mutate(pDat, barcode=as.character(barcode)) %>%
-    mutate(SampleID= do.call(c, mapply(function(.x, .y){
-        rep(.x, .y)
-    }, library_id, numberOfCellperLib, SIMPLIFY = FALSE)))
- 
+}
 
 # Add more info to the feature Data
 ## extract mitochondrial genes
-## mkdir -p 
-## grep '^MT' Sus_scrofa.Sscrofa11.1.92.gtf | grep -o 'ENSSSCG[0-9]*' | uniq > ./results/R.out/data/miscData/MitoGenes.txt
+gtf <- "/home/haibol/haibol_work/haibo_genome/sus_scrofa/Sus_scrofa.Sscrofa11.1.97.gtf"
 
-mitoGenes <- read.table("./results/R.out/data/miscData/MitoGenes.txt")
+mitoGenes <-  system2("grep", 
+                    args = c('^MT', gtf, "| grep -o 'ENSSSCG[0-9]*' | uniq"), 
+                    stdout = TRUE)
 
-# tfCheck <- read.table("../data/miscData/TFcheckpoint_WithENSID.tsv",
-#                       header=TRUE, sep="\t")
-
-fDat$Mitochondrial <- fDat$id %in% mitoGenes$V1
+fDat$Mitochondrial <- fDat$EnsemblID %in% mitoGenes
 
 
 # Save data
-stopifnot(identical(rownames(fDat),rownames(cDat)) & identical(colnames(cDat),pDat$barcode))
-DataList <- list("phenoData"=pDat, "featureData"=fDat, "counts"=cDat)
+stopifnot(identical(rownames(fDat),rownames(cDat)) & 
+          identical(colnames(cDat),pDat$barcode))
+DataList <- list("phenoData"= pDat, "featureData"=fDat, "counts"=cDat)
 
-out_objects_dir <- "./results/R.out/data/Robjects"
-if(!dir.exists(out_objects_dir)) {dir.create(out_objects_dir)}
+out_objects_dir <- "./results3.0/R.out/data/Robjects"
+if(!dir.exists(out_objects_dir)) {dir.create(out_objects_dir, recursive = TRUE)}
 
 saveRDS(DataList,file=file.path(out_objects_dir, "ExpressionList.rds"))
